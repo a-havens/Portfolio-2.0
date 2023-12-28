8d9adfd1b37cd79fac0a5b748ffc428301f45928
a-havens <amanda.havens1@yahoo.com>
2023-12-27 23:30:00 -0500

    added new GitHub URL

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -93,7 +93,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/a-havens/react-portfolio"
+                href="https://github.com/a-havens/Portfolio-2.0"
                 target="_blank"
                 className="fork-btn-inner"
               >
