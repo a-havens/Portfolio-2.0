e3bd2e98688abb8d8aacbea379afe650577b0398
Amanda <amanda.havens1@yahoo.com>
2024-01-26 15:35:27 -0500

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,37 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
+import Particle from "../Particle";
+import pdf from "../../Assets/amanda-havens-resume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-import Particle from "../Particle";
-import pdf from "../../Assets/amanda-havens-resume.pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
-  const [width, setWidth] = useState(window.innerWidth);
-  const [numPages, setNumPages] = useState(null);
+  const [width, setWidth] = useState(1200);
 
   useEffect(() => {
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  const handleResize = () => {
     setWidth(window.innerWidth);
-  };
-
-  const onDocumentLoadSuccess = ({ numPages }) => {
-    setNumPages(numPages);
-  };
+  }, []);
 
   return (
     <div>
       <Container fluid className="resume-section">
-      <Particle />
+        <Particle />
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
@@ -39,25 +26,24 @@ function ResumeNew() {
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
+            <AiOutlineDownload />
             &nbsp;Download CV
           </Button>
         </Row>
+
         <Row className="resume">
-        <Document
-        file={pdf}
-        className="d-flex justify-content-center"
-        onLoadSuccess={onDocumentLoadSuccess}
-        >
-            <Page pageNumber={1} width={600} height={800} scale={1.5} />
+          <Document file={pdf} className="d-flex justify-content-center">
+            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
+
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
             href={pdf}
             target="_blank"
             style={{ maxWidth: "250px" }}>
-
+              
             <AiOutlineDownload />
             &nbsp;Download CV
           </Button>
