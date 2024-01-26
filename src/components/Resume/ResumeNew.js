4c868658379999f1dae29a2706c4b5f0274e5c39
Amanda Havens <128727849+a-havens@users.noreply.github.com>
2024-01-26 15:01:07 -0500

    revert ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -43,21 +43,33 @@ function ResumeNew() {
   return (
     <div>
       <Container fluid className="resume-section">
-        {/* ... Other content ... */}
-
-        <Row className="resume">
-          <Document
-            file={pdfFile}
-            className="d-flex justify-content-center"
-            onLoadSuccess={onDocumentLoadSuccess}
+      <Particle />
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={pdf}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
           >
-            <Page
-              pageNumber={1}
-              width={pageWidth}
-              height={pageHeight}
-              scale={scale}
-            />
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+        <Row className="resume">
+          <Document file={pdf} className="d-flex justify-content-center">
+              <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
+        </Row>
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={pdf}
+            target="_blank"
+            style={{ maxWidth: "250px" }}>
+
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
         </Row>
       </Container>
     </div>
