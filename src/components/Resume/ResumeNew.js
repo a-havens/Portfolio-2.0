687f78c8fc520d5fa0d47e7a53c9b09164b4002c
Amanda <amanda.havens1@yahoo.com>
2024-02-23 09:25:15 -0500

    Still trying to fix resume page issue

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -3,13 +3,12 @@ import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import pdf from "../../Assets/amanda-havens-resume.pdf";
-import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
-  const [width, setWidth] = useState(1200);
+  const [setWidth] = useState(1200);
 
   useEffect(() => {
     setWidth(window.innerWidth);
@@ -34,8 +33,7 @@ function ResumeNew() {
             file={pdf} 
             className="d-flex justify-content-center">
               <Page 
-                pageNumber={1} 
-                scale={width > 786 ? 1.7 : 0.6} />
+                pageNumber={1} />
           </Document>
         </Row>
       </Container>
