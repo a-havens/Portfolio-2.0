a1d5f5d7c9960bc37659782599dac4aa09755617
Amanda <amanda.havens1@yahoo.com>
2024-02-23 09:03:23 -0500

    Removed second Download CV button

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -38,17 +38,6 @@ function ResumeNew() {
                 scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
-
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}>
-            &nbsp;Download CV
-          </Button>
-        </Row>
-
       </Container>
     </div>
   );
